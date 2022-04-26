b02b6a07f220a4185a1549193794c2a9d3e6e25a
vidya2906 <101714184+vidya2906@users.noreply.github.com>
2022-04-27 03:29:11 +0530

    Add files via upload

---
+++ 3.Alerts, Frame & Windows/1.Browser_windows/Program.cs
@@ -0,0 +1,80 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _1.Browser_windows
+{
+    internal class Program
+    {
+        private static string p;
+        private static string c;
+        static void Main(string[] args)
+        {
+            Console.Write("Test Case Started:");
+
+            IWebDriver driver = new ChromeDriver();
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+
+            driver.Navigate().GoToUrl("https://demoqa.com/browser-windows");
+
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(2000);
+
+            newTabBTN(driver);
+            Thread.Sleep(1500);
+
+            newWindowBTN(driver);
+            Thread.Sleep(1500);
+
+            newWindow_msgBTN(driver);
+
+            Task.Delay(120000).Wait();
+
+            Console.Write("Test Case Closed:");
+
+            driver.Close();
+            driver.Quit();
+        }
+
+        static void newTabBTN(IWebDriver Driver)
+        {
+            Driver.FindElement(By.XPath("//button[@id='tabButton']")).Click();
+            Thread.Sleep(2000);
+            p = Driver.WindowHandles[0];
+            c = Driver.WindowHandles[1];
+            Driver.SwitchTo().Window(c);
+            Driver.Close();
+            Thread.Sleep(2000);
+            Driver.SwitchTo().Window(p);
+        }
+
+        static void newWindowBTN(IWebDriver Driver)
+        {
+            Driver.FindElement(By.XPath("//button[@id='windowButton']")).Click();
+            Thread.Sleep(2000);
+            c = Driver.WindowHandles[1];
+            Driver.SwitchTo().Window(c);
+            Driver.Close();
+            Thread.Sleep(2000);
+            Driver.SwitchTo().Window(p);           
+            Thread.Sleep(2000);
+        }
+
+        static void newWindow_msgBTN(IWebDriver Driver)
+        {
+            Driver.FindElement(By.XPath("//button[@id='messageWindowButton']")).Click();
+            Thread.Sleep(2000);
+            c = Driver.WindowHandles[1];
+            Driver.SwitchTo().Window(c);
+            Driver.Close();
+            Thread.Sleep(2000);
+            Driver.SwitchTo().Window(p);
+            Thread.Sleep(2000);
+        }
+    }
+}
