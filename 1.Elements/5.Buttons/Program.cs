d87e904f665103a22fd005fac1368a752e0e2122
vidya2906 <101714184+vidya2906@users.noreply.github.com>
2022-04-27 03:20:30 +0530

    Add files via upload

---
+++ 1.Elements/5.Buttons/Program.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _5.Buttons
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Test Case Started:");
+
+            IWebDriver driver = new ChromeDriver();
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+
+            driver.Navigate().GoToUrl("https://demoqa.com/buttons");
+
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(2000);
+
+            buttons(driver);
+
+            Console.Write("Test Case Closed:");
+
+            Task.Delay(120000).Wait();
+
+            driver.Close();
+            driver.Quit();
+        }
+
+        static void buttons(IWebDriver Driver)
+        {
+            Actions a = new Actions(Driver);
+
+            a.MoveToElement(Driver.FindElement(By.Id("doubleClickBtn"))).DoubleClick().Build().Perform();
+            Thread.Sleep(2000);
+
+            a.MoveToElement(Driver.FindElement(By.Id("rightClickBtn"))).ContextClick().Build().Perform();
+            Thread.Sleep(2000);
+
+            Driver.FindElement(By.XPath("//*[text()='Click Me']")).Click();
+        }
+    }
+}
